7eb15e2df7ada7fe80d7116afc3dbd605b3ecf59
Alex S <habahabahaba@outlook.com>
2024-08-21 07:41:57 +0400

    Edited NewSnippetPage and layout

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -11,7 +11,37 @@ interface NewSnippetPageProps {}
 
 const NewSnippetPage: FC<NewSnippetPageProps> = () => {
   // JSX:
-  return <div> Create a New Snippet Here</div>;
+  return (
+    <form>
+      <h3 className='font-bold m-3'>Add Snippet</h3>
+      <div className='flex flex-col gap-4'>
+        <div className='flex gap-4'>
+          <label className='w-12' htmlFor='title'>
+            Title
+          </label>
+          <input
+            type='text'
+            name='title'
+            className='border rounded p-2 w-full'
+            id='title'
+          />
+        </div>
+        <div className='flex gap-4'>
+          <label className='w-12' htmlFor='code'>
+            Code
+          </label>
+          <textarea
+            name='code'
+            className='border rounded p-2 w-full'
+            id='code'
+          />
+        </div>
+        <button type='submit' className='rounded p-2 bg-blue-200'>
+          Add
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default NewSnippetPage;
