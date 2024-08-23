c21fa213369676c5f078c8f98fe06b88709faa5f
Alex S <habahabahaba@outlook.com>
2024-08-23 07:27:56 +0400

    Worked on the Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { db } from '@/db';
 // Redux RTK:
 // Store:
 // Next:
+import Link from 'next/link';
 // React:
 // Context:
 // Components:
@@ -15,8 +16,25 @@ export default async function Home() {
 
   // JSX:
   const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>{snippet.title}</div>
+    <Link
+      key={snippet.id}
+      href={`/snippets/${snippet.id}`}
+      className='flex justify-between items-center py-2 px-3 border rounded'
+    >
+      {snippet.title}
+      <div>View</div>
+    </Link>
   ));
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className='flex m-2 justify-between items-center'>
+        <h1 className='text-xl font-bold'>Snippets</h1>
+        <Link href='/snippets/new' className='border p-2 rounded'>
+          New
+        </Link>
+      </div>
+      <div className='flex flex-col gap-2'>{renderedSnippets}</div>
+    </div>
+  );
 }
