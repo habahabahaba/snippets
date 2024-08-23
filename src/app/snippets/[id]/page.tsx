eb520a7bb9a6faf48400400c6b62dcf9b1315b7c
Alex S <habahabahaba@outlook.com>
2024-08-23 06:58:43 +0400

    Worked further on the SnippetShowPage

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,7 +1,9 @@
 // 3rd party:
+import { db } from '@/db';
 // Redux RTK:
 // Store:
 // Next:
+import { notFound } from 'next/navigation';
 // React:
 // Context:
 // Components:
@@ -9,17 +11,27 @@
 // Types, interfaces and enumns:
 import { FC } from 'react';
 interface SnippetShowPageProps {
-  params: any;
+  params: { id: string };
   searchParams?: any;
 }
 
-const SnippetShowPage: FC<SnippetShowPageProps> = ({
+const SnippetShowPage: FC<SnippetShowPageProps> = async ({
   params,
   searchParams,
 }) => {
   console.log(params, searchParams);
+  //   Fetching the snippet:
+  const snippet = await db.snippet.findFirst({
+    where: { id: +params.id },
+  });
+
+  // If there's no such snippet:
+  if (!snippet) return notFound();
+
   // JSX:
-  return <div>{`Look, a Snippet with the id of ${params.id} !!!`}</div>;
+  return (
+    <div>{`Look, it's a Snippet with the id of ${params.id} and the title of: "${snippet.title}" !!!`}</div>
+  );
 };
 
 export default SnippetShowPage;
